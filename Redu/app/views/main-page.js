209d53d605ad008030dede3a5aa163171024eeff
Maskln <lyubomir.nanev@gmail.com>
2016-02-20 14:14:50 +0200

    Added Images, .jshintrc and Main page stylization

--- Redu/app/views/main-page.js
+++ Redu/app/views/main-page.js
@@ -6,4 +6,32 @@ function pageLoaded(args) {
     page.bindingContext = vmModule.mainViewModel;
 }
 
+function onStartBtnTapped() {
+  console.log("start button tapped");
+  //change backgroud image to pressed;
+
+  // frame.topmost()
+  //   .navigate('game-page');
+}
+
+function onLeaderboardBtnTapped() {
+  console.log("leaderboard button tapped");
+  //change backgroud image to pressed;
+
+  // frame.topmost()
+  //   .navigate('global-leaderboard-page');
+}
+
+function onSettingsBtnTapped() {
+  console.log("settings button tapped");
+  //change backgroud image to pressed;
+
+  // frame.topmost()
+  //   .navigate('settings-page');
+}
+
+
 exports.pageLoaded = pageLoaded;
+exports.onStartBtnTapped = onStartBtnTapped;
+exports.onLeaderboardBtnTapped = onLeaderboardBtnTapped;
+exports.onSettingsBtnTapped = onSettingsBtnTapped;
