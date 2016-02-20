409417842156909d7709c0695bdeb882de954d2b
Nicky <newmast@abv.bg>
2016-02-20 13:56:20 +0300

    removed unnecessary files

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-let fs = require('fs');
-
-module.exports = function(app) {
-  let path = './routers';
-  fs.readdirSync(path)
-    .filter(file => file !== 'index.js')
-    .forEach(file => require(`./${file}`)(app));
-}
