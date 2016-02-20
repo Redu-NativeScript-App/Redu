9a67a8497f02ed1faa69956e82452c570170f275
Maskln <lyubomir.nanev@gmail.com>
2016-02-20 17:59:25 +0200

    All the buttons in Main menu have pressed effect

--- Redu/app/views/main-page.js
+++ Redu/app/views/main-page.js
@@ -1,30 +1,45 @@
 var vmModule = require("../view-models/main-view-model");
+var view = require("ui/core/view");
 var frame = require("ui/frame");
 var topmost;
+var startBtn;
+var leaderboardBtn;
+var settingsBtn;
+var btnPressed = "url('~/images/green-rect-btn-pressed.png')";
+var btnUnpressed = "url('~/images/green-rect-btn-unpressed.png')";
 
 function pageLoaded(args) {
     var page = args.object;
     page.bindingContext = vmModule.mainViewModel;
     topmost = frame.topmost();
+    startBtn = view.getViewById(page, "startBtn");
+    leaderboardBtn = view.getViewById(page, "leaderboardBtn");
+    settingsBtn = view.getViewById(page, "settingsBtn");
+    changeButtonStateIfPressed(startBtn);
+    changeButtonStateIfPressed(leaderboardBtn);
+    changeButtonStateIfPressed(settingsBtn);
 }
 
 function onStartBtnTapped() {
-  //change background image to pressed;
-
+  startBtn.style.backgroundImage = btnPressed;
   topmost.navigate("./views/game-page");
 }
 
 function onLeaderboardBtnTapped() {
-  //change background image to pressed;
-
+  leaderboardBtn.style.backgroundImage = btnPressed;
   topmost.navigate("./views/global-leaderboard-page");
 }
 
 function onSettingsBtnTapped() {
-  //change backgroud image to pressed;
+  settingsBtn.style.backgroundImage = btnPressed;
+  topmost.navigate("./views/settings-page");
+}
 
-  topmost.navigate("./views/settings-page");}
-
+function changeButtonStateIfPressed(button) {
+  if (button.style.backgroundImage === btnPressed) {
+    button.style.backgroundImage = btnUnpressed;
+  }
+}
 
 exports.pageLoaded = pageLoaded;
 exports.onStartBtnTapped = onStartBtnTapped;
