bd9d8b5fda108ad44f064afe1f371f088b4884a6
Maskln <lyubomir.nanev@gmail.com>
2016-02-23 02:50:43 +0200

    Added pressed effect and sounds on difficulty buttons

--- Redu/app/views/choose-difficulty-page.js
+++ Redu/app/views/choose-difficulty-page.js
@@ -1,8 +1,18 @@
 var frame = require('ui/frame');
+var sounds = require('../sounds').sounds;
+var helpers = require('../helpers').helpers;
 var topmost = frame.topmost();
+var easyBtn;
+var mediumBtn;
+var hardBtn;
+var btnPressed = "url('~/images/green-rect-btn-pressed.png')";
 
 function pageLoaded(args) {
-    var page = args.object;
+  var page = args.object;
+  easyBtn = page.getViewById("easyBtn");
+  mediumBtn = page.getViewById("mediumBtn");
+  hardBtn = page.getViewById("hardBtn");
+
 }
 
 function navigateWithColumnCount(numberOfColumns) {
@@ -17,14 +27,23 @@ function navigateWithColumnCount(numberOfColumns) {
 }
 
 function onEasyBtnTapped() {
+  sounds.playSound("Click");
+  easyBtn.style.backgroundImage = btnPressed;
+  helpers.changeButtonStateIfPressed(easyBtn);
   navigateWithColumnCount(4);
 }
 
 function onMediumBtnTapped() {
+  sounds.playSound("Click");
+  mediumBtn.style.backgroundImage = btnPressed;
+  helpers.changeButtonStateIfPressed(mediumBtn);
   navigateWithColumnCount(5);
 }
 
 function onHardBtnTapped() {
+  hardBtn.style.backgroundImage = btnPressed;
+  helpers.changeButtonStateIfPressed(hardBtn);
+  sounds.playSound("Click");
   navigateWithColumnCount(6);
 }
 exports.pageLoaded = pageLoaded;
